cdf11d3266237ca9078e8ed139ecf4bee08be901
yilun.yang <yilun.yang@transwarp.io>
2026-03-02 16:29:00 +0800

    chore: update build info for production

--- lib/build_info.dart
+++ lib/build_info.dart
@@ -1,6 +1,6 @@
 /// Build information - auto-generated at build time
 class BuildInfo {
   static const String version = '1.0.0+1';
-  static const String commit = '2b8b06d';
-  static const String buildTime = '2026-03-02 08:23:46 UTC';
+  static const String commit = '69a1906';
+  static const String buildTime = '2026-03-02 08:28:05 UTC';
 }
